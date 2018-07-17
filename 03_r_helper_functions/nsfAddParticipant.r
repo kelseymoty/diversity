3e0ef84617e4fb0294313fea435f02153c169601
Kelsey Moty <7917220+kelseymoty@users.noreply.github.com>
2018-07-17 11:23:00 -0400

    reorganizing files

---
+++ 03_r_helper_functions/nsfAddParticipant.r
@@ -0,0 +1,35 @@
+library(readr)
+
+nsf_add_participant <- function(data){
+  # initializing variables
+  # creating new participant ID variable
+  # we need a counter (x) to keep track of which participant number we are on
+  # we need a list to keep track of the animals that participants have seen
+  data$participant <- 1
+  x <- 1
+  animalList <- c()
+  
+  # loop to assign participant number
+  for (i in 1:as.integer(nrow(data))){
+    if (i == 1){
+      # on first row, set participant to 1; append animalList with animal viewed
+      data$participant[i] <- x
+      animalList <- append(animalList, data$trial[i])
+    } else if (data$trial[i] %in% animalList | data$age[i] != data$age[i-1] | 
+               data$study[i] != data$study[i-1] | data$condition[i] != data$condition[i-1]){
+      # if animal viewed on this trial has already been viewed (i.e., is in animalList)
+      # or age, condition, or study is different from previous row
+      # move counter up by 1, set participant ID with new x, reset and append animalList
+      x <- x + 1
+      data$participant[i] <- x
+      animalList <- c()
+      animalList <- append(animalList, data$trial[i])
+    } else {
+      # if same age, study, and condition AND animal hasn't already been viewed by participant
+      # set participant ID with current x and append animalList
+      data$participant[i] <- x
+      animalList <- append(animalList, data$trial[i])
+    }
+  }
+  return(data)
+}
